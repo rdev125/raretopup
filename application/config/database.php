9784ab502b0f04cf811a0fc8cb67d1879f44d495
mrohmani <m.rohmani96@gmail.com>
2021-09-24 15:20:58 +0700

    swagger

--- application/config/database.php
+++ application/config/database.php
@@ -76,6 +76,7 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
+	'port'     => '3307',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'project_raretopup',
